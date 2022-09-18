61df030f74b7871f7cc5bd75b79af1b9ce0a9db6
termoTORMOZ <mikitko.batura@gmail.com>
2022-09-18 12:40:36 +0300

    refactoring made by cmake tools

---
+++ list/ent.h
@@ -0,0 +1,8 @@
+#ifndef FUNCT
+#define FUNCT
+#include <iostream>
+
+void entering ();
+void output (int n, int dig);
+
+#endif //FUNCT
---
+++ list/outp.cpp
@@ -0,0 +1,7 @@
+#include "ent.h"
+
+void output(int n, int dig)
+{
+    n = n / dig;
+    std::cout << n << std::endl;
+}
